0bc86ba369a2a275a48f453c925d19bde4b19888
Jake Le <lejake888@gmail.com>
2019-12-10 10:55:33 +0000

    Player moves and lines up at bottom

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 let player = {
-    top: 0,
-    left: 0,
+    top: 585,
+    left: 420,
 }
 
 const move = () => {
     document.getElementById('player').style.left = player.left + "px"
+    document.getElementById('player').style.top = player.top + "px"
 }
 
 document.onkeydown = (e) => {
     if (e.keyCode === 37){
-         player.left = player.left - 10
+         player.left = player.left - 25
          console.log("left")
          move()
     }
     else if(e.keyCode === 39){
-        player.left = player.left + 10
+        player.left = player.left + 25
         console.log("right")
         move()
     }
+}
+
+window.onload() = () => {
+    document.getElementById('player').style.left = player.left + "px"
+    document.getElementById('player').style.top = player.top + "px"
 }
