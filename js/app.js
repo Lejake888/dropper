a80d75a5cea47d4fa1805552ece52dec4561e197
Jake Le <lejake888@gmail.com>
2019-12-10 10:12:00 +0000

    player moves

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 let player = {
-    top: 750,
-    left: 500,
+    top: 0,
+    left: 0,
 }
 
+const move = () => {
+    document.getElementById('player').style.left = player.left + "px"
+}
+
+document.onkeydown = (e) => {
+    if (e.keyCode === 37){
+         player.left = player.left - 10
+         console.log("left")
+         move()
+    }
+    else if(e.keyCode === 39){
+        player.left = player.left + 10
+        console.log("right")
+        move()
+    }
+}
